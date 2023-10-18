c82628b9d7d0629961c180d45d238abc6696280d
mardomai <mardo.mai@ametikool.ee>
2023-10-18 16:01:10 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+$host = '';
+$db   = '';
+$user = '';
+$pass = '';
+$charset = '';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+$pdo = new PDO($dsn, $user, $pass, $options);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello,PHP!</title>
+</head>
+<body>
+    
+<ul>
+    <?php
+    while($row = $stmt fetch()) {
+?>
+
+
+    }
+</ul>
