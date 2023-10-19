f0bff844ad96bb10b094d4372ef3290abf13633b
mardomai <mardo.mai@ametikool.ee>
2023-10-19 13:55:25 +0300

    yes

--- book.php
+++ book.php
@@ -6,3 +6,14 @@ $id = $_GET['id'];
 
 $sql = "SELECT * FROM books WHERE id = {$ID}";
 
+$stmt = $pdo->prepare('SELECT * FROM users WHERE email = ? AND status=?');
+$stmt->execute([$email, $status]);
+$user = $stmt->fetch();
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+$pdo = new PDO($dsn, $user, $pass, $options);
